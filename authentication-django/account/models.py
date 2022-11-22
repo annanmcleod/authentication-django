97b7ed69964f5c0875fa4e2c0f37894729499144
annanmcleod <111473493+annanmcleod@users.noreply.github.com>
2022-11-22 16:34:01 -0600

    Add files via upload

---
+++ authentication-django/account/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+# import uuid
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    birthdate = models.CharField(max_length=200, null=True)
+    gender = models.CharField(max_length=200, null=True)
+    is_active = models.CharField(max_length=200, null=True)
+    data_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
